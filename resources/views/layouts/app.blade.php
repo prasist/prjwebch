b649e8c43ecd71779b6142a7e44edb61bd6b2b27
Fabiano <oscarfabiano@hotmail.com>
2016-03-01 14:36:13 -0300

    atualizacao layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Web Igrejas 1.0</title>
+    <title>SIGMA3 - Gestão de Igrejas</title>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
@@ -18,6 +18,7 @@
     <style>
         body {
             font-family: 'Lato';
+            background-image: url('http://177.101.149.118/wp-content/uploads/2016/02/online-church-sruvey-checklist-bg.jpg');
         }
 
         .fa-btn {
@@ -39,8 +40,8 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Web Igrejas 1.0
+                <a class="navbar-brand" href="{{ url('http://177.101.149.118') }}">
+                    SIGMA3 - Gestão de Igrejas
                 </a>
             </div>
 
