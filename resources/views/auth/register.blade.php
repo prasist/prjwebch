c477645646c56b6717e2f218385e272b4ba5af23
Fabiano <oscarfabiano@hotmail.com>
2016-03-15 17:44:00 -0300

    melhorias tela login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,8 +5,16 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
+
                 <div class="panel-heading">Cadastro</div>
                 <div class="panel-body">
+
+                <p>
+                    <b>Importante !!! </b><br/>
+                    Utilize um endereço de e-mail válido, pois precisará dele para efetuar o login.<br/><br/>
+
+                    Após o preenchimento do formulário você receberá um e-mail com o link de validação. Caso não receba,  verifique na caixa de SPAM do seu e-mail.<br/>
+                </p>
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {!! csrf_field() !!}
 
@@ -14,7 +22,7 @@
                             <label class="col-md-4 control-label">Nome</label>
 
                             <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                                <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Seu Nome">
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -24,11 +32,13 @@
                             </div>
                         </div>
 
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label class="col-md-4 control-label">E-Mail</label>
 
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                    <input type="email" class="form-control" placeholder="Informe um e-mail válido, ele será seu login" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -36,13 +46,14 @@
                                     </span>
                                 @endif
                             </div>
+
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label class="col-md-4 control-label">Senha</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
+                                <input type="password" class="form-control" name="password" placeholder="Mínimo 6 dígitos">
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -56,7 +67,7 @@
                             <label class="col-md-4 control-label">Confirmar Senha</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
+                                <input type="password" class="form-control" name="password_confirmation" placeholder="Confirme a senha">
 
                                 @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
