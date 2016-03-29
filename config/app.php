7dbd247f52cd2db335dc1104b3e055858fccc70e
Fabiano <oscarfabiano@hotmail.com>
2016-03-29 18:22:36 -0300

    implementacao fotos 2

--- config/app.php
+++ config/app.php
@@ -88,7 +88,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         Barryvdh\Debugbar\ServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -135,7 +135,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Image' => 'Intervention\Image\Facades\Image',
-        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
