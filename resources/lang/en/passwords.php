dc87df84bb33f820814056f2afea943e7617c337
Fabiano <oscarfabiano@hotmail.com>
2016-04-25 17:45:37 -0300

    atualizacao email

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -14,9 +14,9 @@ return [
     */
 
     'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'reset' => 'Atualização de Senha!',
+    'sent' => 'Você receberá em breve um email com o link para gerar a nova senha!',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "Usuário não encontrado com o email informado.",
 
 ];
