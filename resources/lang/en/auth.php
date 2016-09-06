be7e214a9655cc11d702297cb5f454b0aba35852
Fabiano <oscarfabiano@hotmail.com>
2016-09-06 08:58:43 -0300

    language

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Dados não conferem. Verifique se o email ou senha estão corretos.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
